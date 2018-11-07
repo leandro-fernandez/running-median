7ec72d61b9263830e833e6576fa10a04fdfd9bb8
Fernandez <af.leandro@gmail.com>
2018-11-07 15:47:58 -0200

    Initial version. It's not working for some cases yet.

---
+++ RunningMedian/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RunningMedian
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
